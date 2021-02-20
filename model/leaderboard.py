915683590499f257da21a0fb21c69ee8cc7ae6e0
cansuulker <culker@sabanciuniv.edu>
2021-02-21 01:32:52 +0300

    Requirements.txt updated

--- model/leaderboard.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import *
-
-class leaderboard(Document):
-    rank = IntField(required=True)
-    points = IntField(required=True)
-    display_name = StringField(required=True,primary_key=True,unique=True)
-    country = StringField()
-
-
