35a93fae81906273196157b085cab117eaa5df60
cansuulker <culker@sabanciuniv.edu>
2021-02-21 00:19:43 +0300

    Procfile update

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ if not MONGO_URL:
     MONGO_URL = 'mongodb+srv://' + urllib.parse.quote('root') + ':' + urllib.parse.quote('aD1gEy5BsNJFG2Wy') + '@cluster0.cci75.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 db = MongoEngine(app=flask_app)
-db.init_app(app)
 
 if __name__ == '__main__':
     flask_app.run(debug=True)
