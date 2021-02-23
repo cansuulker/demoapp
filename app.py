e0f97c11209e06885c49690cae6e58ecc36d2aba
cansuulker <culker@sabanciuniv.edu>
2021-02-24 00:28:14 +0300

    Heroku deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import mongoengine
+import urllib
 from mongoengine.connection import get_db, connect
 from flask import Flask, app
 from flask_restful import Resource, Api
@@ -13,12 +14,12 @@ from api.userapi import usersapi
 from api.routes import create_routes
 
 
-default_config = {'MONGODB_SETTINGS': {
+'''default_config = {'MONGODB_SETTINGS': {
     'db': 'test',
     'host': 'localhost',
     'port': 27017,
 
-}}
+}}'''
 
 heroku_config = {'MONGODB_SETTINGS:': {
     'db': 'users',
@@ -27,22 +28,23 @@ heroku_config = {'MONGODB_SETTINGS:': {
 
 # init flask
 app = Flask(__name__)
-
 api = Api(app=app)
 
 #create endpoints
 create_routes(api=api)
 
 MONGO_URI = os.environ.get('MONGO_URI')
+if not MONGO_URI:
+     MONGO_URI = 'mongodb+srv://' + urllib.parse.quote('admin') + ':' + urllib.parse.quote('arU1TensYAUHbzVB') + '@gjgapi.wfht1.mongodb.net/users?retryWrites=true&w=majority'
 
 print(MONGO_URI)
-if not MONGO_URI:
-    config = default_config
+'''if not MONGO_URI:
+    config = heroku_config
     app.config.update()
 
 else:
     config = heroku_config
-    app.config.update()
+    app.config.update()'''
 
 db = MongoEngine()
 db.init_app(app)
